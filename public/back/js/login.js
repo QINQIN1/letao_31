b2ab4343c908be66a9f8b8a51cd0cb5e5adf6a81
QINQIN1 <941054391@qq.com>
2018-11-27 20:07:03 +0800

    校验提示完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -25,6 +25,9 @@ $(function () {
                         min:2,
                         max:6,
                         message:"用户名长度必须是2-6位"
+                    },
+                    callback:{
+                        message:"用户名不存在"
                     }
                 }
             },
@@ -39,6 +42,9 @@ $(function () {
                         min:6,
                         max:12,
                         message:"密码长度必须是6-12位"
+                    },
+                    callback:{
+                        message:"密码不存在"
                     }
                 }
             }
@@ -50,7 +56,7 @@ $(function () {
     //表单校验成功之后，默认的是表单提交，我们要阻止默认跳转，用ajax发送请求提交数据
 //     // 表单提交成功，会触发success.form.bv事件
     $('#form').on('success.form.bv', function( e ) {
-        console.log(e)
+        // console.log(e)
         //阻止表单的自动提交
         e.preventDefault();
     // console.log( "默认的表单提交被阻止了, 通过ajax来提交" )
@@ -64,11 +70,19 @@ $(function () {
             success:function(info){
                 console.log(info);
                 if(info.error==1000){
-                    alert('用户名不存在');
+                    // alert('用户名不存在');
+                    //updateStatus
+                    //参数 
+                    // field字段名 
+                    // status状态 
+                    // NOT_VALIDATED未验证 VALIDATING验证中 INVALID验证失败 VALID 验证成功
+                    $('#form').data("bootstrapValidator").updateStatus("username","INVALID","callback")
                     return;
+                    
                 }
                 if(info.error==1001){
-                    alert('密码错误');
+                    // alert('密码错误');
+                    $('#form').data("bootstrapValidator").updateStatus("password","INVALID","callback")
                     return;
                 }
                 if(info.success){
